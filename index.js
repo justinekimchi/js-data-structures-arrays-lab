6d3004837284a1f5c40dbe4a4026fa359502f2eb
justinekimchi <justine.kim.manaloto@gmail.com>
2020-11-29 16:48:03 -0600

    commit tree b5281dd17b8e357434ab24a96cdd6ea4e6bd8853

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift(name);
+}
+
+function appendDriver(name) {
+  const newDriverList = [...drivers, name];
+  return newDriverList;
+}
+
+function prependDriver(name) {
+  const newDriverList2 = [name, ...drivers];
+  return newDriverList2;
+}
+
+function removeLastDriver(name) {
+  const removeDriver = drivers.slice(0, drivers.length - 1);
+  return removeDriver;
+}
+
+function removeFirstDriver(name) {
+  const removeDriver1 = drivers.slice(1);
+  return removeDriver1;
+}
